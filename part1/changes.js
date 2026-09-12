eadf3af64030a847ff9e8a2496d73c99d13a961b
Shiv-Shankar55 <sibsankarpanda555@gmail.com>
2026-09-12 19:29:56 +0530

    first commit

---
+++ part1/changes.js
@@ -0,0 +1,8 @@
+let gameName = "bgmi";
+gameName = "COC";
+console.log(gameName); //In this case overridden is happening
+
+
+const user = "shiv";
+user = "siba";
+console.log(user); //In this case once a value is assigned nothing will be overridden and give error
---
+++ part1/datatype.js
@@ -0,0 +1,30 @@
+console.log("Hello");
+// console.log("Chai");
+/*
+    Datatypes : 
+    
+    String
+    Number
+    Boolean
+    Bigint
+
+    Undefined
+    null
+
+    Object
+
+    Symbols
+*/
+
+// var score = 120
+let score = 120
+let name = "shiv";
+let isLoggedIn = false;
+
+//Object
+let teaTypes = ["Lemon Tea","Orange Tea","Oolong Tea"];
+let user = { firstname: "Shiv" , lastname: "Shankar"};
+
+let getScore = score;
+
+console.log(getScore);
